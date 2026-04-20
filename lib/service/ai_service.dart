88551a182b60369d0342de869f1fad37859841a6
Himashmayadunna <154499295+Himashmayadunna@users.noreply.github.com>
2026-04-20 15:34:47 +0530

    fixing dependancies error

--- lib/service/ai_service.dart
+++ lib/service/ai_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class GeminiService {
   static const String _apiKey =
-      "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";
+      "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";
   static const String _baseUrl =
       "https://openrouter.ai/api/v1/chat/completions";
   static const String _model = "deepseek/deepseek-r1-0528";
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
+  geolocator_windows
   speech_to_text_windows
 )
 
